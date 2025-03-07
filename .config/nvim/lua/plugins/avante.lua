c593001024009f79d9326167057b6d4fa7966810
Lu Dinh Quan <ludinhquan@gmail.com>
2025-03-07 07:38:41 +0700

    feat: update import path avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,15 +1,14 @@
 return {
 	provider = "openai",
 	openai = {
-		-- model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
-		model = "o3-mini", -- your desired model (or use gpt-4o, etc.)
+		model = "o3-mini",
 	},
 	vendors = {
 		deepseek = {
 			__inherited_from = "openai",
 			endpoint = "https://api.deepseek.com",
 			model = "deepseek-coder",
-			api_key_name = "DEEPSEEK_API_KEY", -- Get API key from environment variable
+			api_key_name = "DEEPSEEK_API_KEY",
 		},
 	},
 }
