1e0775b0047c4d7f240a84681c465cd695275213
quan <quan@192.168.1.96>
2026-01-17 15:15:34 +0700

    feat: update config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,14 +1,2 @@
 return {
-	provider = "openai",
-	openai = {
-		model = "o3-mini",
-	},
-	vendors = {
-		deepseek = {
-			__inherited_from = "openai",
-			endpoint = "https://api.deepseek.com",
-			model = "deepseek-coder",
-			api_key_name = "DEEPSEEK_API_KEY",
-		},
-	},
 }
