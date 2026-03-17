8543ed9fb856d03ab29e619a9f2ceb7c19133849
ludinhquan <ludinhquan@gmail.com>
2026-03-17 07:57:53 +0700

    feat: update nvim plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -327,14 +327,14 @@ local plugins = {
 					},
 				},
 			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
+			-- {
+			-- 	-- Make sure to set this up properly if you have lazy=true
+			-- 	"MeanderingProgrammer/render-markdown.nvim",
+			-- 	opts = {
+			-- 		file_types = { "markdown", "Avante" },
+			-- 	},
+			-- 	ft = { "markdown", "Avante" },
+			-- },
 		},
 	},
 }
